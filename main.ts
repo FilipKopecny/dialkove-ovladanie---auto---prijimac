1923f2e96a995be43c108ac52e41f2329375ee7b
Filipko <98056125+FilipKopecny@users.noreply.github.com>
2022-02-14 19:33:40 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
 radio.onReceivedValue(function (name, value) {
-    if (name == "dopredodozadu") {
+    if (name == "dopredud") {
         prijata_dopredudozadu = value
     }
-    if (name == "vpravovlavo") {
+    if (name == "vpravovl") {
         prijata_vpravovlavo = value
     }
 })
 let prijata_vpravovlavo = 0
 let prijata_dopredudozadu = 0
+radio.setGroup(1)
 let lavy_motor = 0
 let pravy_motor = 0
 prijata_dopredudozadu = 512
@@ -19,6 +20,12 @@ basic.forever(function () {
     1023,
     -255,
     255
+    ) + pins.map(
+    prijata_vpravovlavo,
+    0,
+    1023,
+    -255,
+    255
     )
     pravy_motor = pins.map(
     prijata_dopredudozadu,
@@ -26,6 +33,24 @@ basic.forever(function () {
     1023,
     -255,
     255
+    ) + pins.map(
+    prijata_vpravovlavo,
+    0,
+    1023,
+    255,
+    -255
     )
+    if (lavy_motor < -255) {
+        lavy_motor = -255
+    }
+    if (lavy_motor > 255) {
+        lavy_motor = 255
+    }
+    if (pravy_motor < -255) {
+        pravy_motor = -255
+    }
+    if (pravy_motor > 255) {
+        pravy_motor = 255
+    }
     Rover.MotorRunDual(lavy_motor, pravy_motor)
 })
